f54f9e0d792388d9cbc24c2e870ac16c61ed1656
Samuel2779 <44899739+Samuel2779@users.noreply.github.com>
2020-11-17 17:38:40 -0600

    added basic documentation

--- Main.py
+++ Main.py
@@ -1,24 +1,71 @@
+"""Top-Down-parsing tree
+
+This script builds a tree with a top down parsing method and validating
+if a string is accepted or not in the parsing tree
+
+This tool accepts text files (.txt) with the correct syntax to build a 
+parsing tree from a grammar.
+
+This script requires `pptree` be installed within the Python
+environment you are running this script in. 
+
+You can install pptree using command `pip install pptree` in your terminal
+in case you do not have it.
+
+
+"""
 from pptree import *
 
-def TopDownParsing(nonTerminal,terminal,initialState,productions,queue,string,depth,Tree):
+def TopDownParsing(nonTerminal,terminal,initialState,productions,string,depth):
+    """Top down parsing method to build the parsing Tree
+
+    Parameters
+    ----------
+    nonTerminal : list
+        All the non Terminal symbols in the .txt file
+    terminal : list
+        All the terminal symbols in the .txt file
+    initialState: str
+        Initial state in the grammar
+    productions: dict
+        All the productions of the grammar
+    string: str
+        Input string to validate
+    depth: int
+        Input max depth the tree must go   
+
+    Returns
+    -------
+    Printed Tree and string validation 
+    """ 
+
     print("TopDownParsing Method")
-    dicDepth = {}
-    queue.append(initialState)
-    dicDepth[initialState] = 0
+    queue = []
     devide = []
+
+    Tree = {}
+    dicDepth = {}
+
     found = False
     uwv = ""
+
+    queue.append(initialState)
+    dicDepth[initialState] = 0
+
     while queue and not found:
         q = queue.pop(0)
+
         if dicDepth[q] + 1 > depth:
             break
-        i=0
+
         done = False
+
         for x in range(len(q)):
             if q[x].isupper():
                 q = q.partition(q[x])
                 break      
         while not done and not found:
+
             for j in productions[q[1]]:
                 devide.clear()
                 w = j
@@ -27,10 +74,12 @@ def TopDownParsing(nonTerminal,terminal,initialState,productions,queue,string,de
                 devide.append(w)
                 devide.append(q[2])
                 uwv = listToString(devide)
+
                 allTerminal = 0
                 for minus in range(len(uwv)):
                     if uwv[minus] in terminal:
                         allTerminal = allTerminal + 1
+
                 matches = True
                 for x in range(len(checkIfmatches)):
                     if len(checkIfmatches) > len(string):
@@ -39,6 +88,7 @@ def TopDownParsing(nonTerminal,terminal,initialState,productions,queue,string,de
                     elif checkIfmatches[x] != string[x]:
                         matches = False
                         break        
+
                 if allTerminal < len(uwv) and matches:
                     queue.append(uwv)
                     append_value(Tree,listToString(q),uwv)
@@ -53,23 +103,57 @@ def TopDownParsing(nonTerminal,terminal,initialState,productions,queue,string,de
             done = True  
                    
     if found:
-        print("------------------String accepted------------------")
+        print("------------------String " +string + " Accepted------------------")
     else:
-        print("----------------String NOT accepted----------------")   
-    #print nice tree (missing)
+        print("----------------String " +string + " NOT Accepted----------------")   
+
     tempRoot = Node(initialState)
     print_tree_method_recursive(Tree,initialState,tempRoot)
-    print_tree(tempRoot,horizontal=True)
+
+    if depth <= 5:
+        print_tree(tempRoot,horizontal=False)
+    else:
+        print_tree(tempRoot,horizontal=True)
     
+    if found:
+        print("------------------String " +string + " Accepted------------------")
+    else:
+        print("----------------String " +string + " NOT Accepted----------------")
 
 
-def listToString(devide):  
+def listToString(devide): 
+    """Simple list to string converter
+
+    Parameters
+    ----------
+    devide : list
+        The list wanted to construct a str
+
+    Returns
+    -------
+    string constructed 
+    """ 
     str1 = ""    
     for x in devide:  
         str1 += x    
     return str1
 
 def append_value(dict_obj, key, value):
+    """Insert values to a dict in order to have list inside
+
+    Parameters
+    ----------
+    dict_obj : dict
+        The dictionary with current elements
+    key : string
+        The key we want to assign the value
+    value : string
+        The value we want to assign to the key
+
+    Returns
+    -------
+    Dictionary with value added 
+    """ 
     if key in dict_obj:
         if not isinstance(dict_obj[key], list):
             dict_obj[key] = [dict_obj[key]]
@@ -78,38 +162,56 @@ def append_value(dict_obj, key, value):
         dict_obj[key] = [value]
 
 def print_tree_method_recursive(Tree,key,parent):
+    """Print Tree using pprint import
+
+    Parameters
+    ----------
+    Tree : dict
+        Dictionary with all nodes and children
+    key : str
+        String representing a node
+    parent : node
+        The parent to assign children  
+    
+
+    Expected value
+    -------
+    built complete tree 
+    """
     for x in Tree:
         for j in Tree[x]:
             if x == key:
                 nod = Node(j,parent)
                 print_tree_method_recursive(Tree,j,nod)
 
-print("Select the text file (e.g. test1, test2,test3,test4)")
-txtVal = input()
-print("String to evaluate")
-string = input()
-print("indicate the maximun depth")
-depth = int(input())
 
-productions = {}
-queue = []
-Tree = {}
+repeat = True
 
-with open(txtVal + ".txt") as f:
-    nonTerminal = f.readline().split(",")
-    nonTerminal[len(nonTerminal)-1] = nonTerminal[len(nonTerminal)-1].strip()
-    terminal = f.readline().split(",")
-    terminal[len(terminal)-1] = terminal[len(terminal)-1].strip()
-    initialState = f.readline().strip() 
-    for i in range(len(nonTerminal)*3):
-            ste = f.readline()
-            if not ste:
-                break
-            ste = ste.split("->")
-            ste[len(ste)-1] = ste[len(ste)-1].strip()
-            if ste[0].isupper():
-                append_value(productions,ste[0],ste[1])
-            ste.clear()
+while repeat:
+    print("Select the text file (e.g. test1, test2,test3,test4)")
+    txtVal = input()
+    print("String to evaluate")
+    string = input()
+    print("indicate the maximun depth")
+    depth = int(input())
 
-TopDownParsing(nonTerminal,terminal,initialState,productions,queue,string,depth,Tree)
+    productions = {}
+
+    with open(txtVal + ".txt") as f:
+        nonTerminal = f.readline().split(",")
+        nonTerminal[len(nonTerminal)-1] = nonTerminal[len(nonTerminal)-1].strip()
+        terminal = f.readline().split(",")
+        terminal[len(terminal)-1] = terminal[len(terminal)-1].strip()
+        initialState = f.readline().strip() 
+        for i in range(len(nonTerminal)*3):
+                ste = f.readline()
+                if not ste:
+                    break
+                ste = ste.split("->")
+                ste[len(ste)-1] = ste[len(ste)-1].strip()
+                if ste[0].isupper():
+                    append_value(productions,ste[0],ste[1])
+                ste.clear()
+
+    TopDownParsing(nonTerminal,terminal,initialState,productions,string,depth)
 
